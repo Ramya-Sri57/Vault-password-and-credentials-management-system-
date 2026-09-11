fed68e3b10e6ccc8cb898f7337f2a70b9585cf3d
Ramya Sri <rramyasrikadimi@gmail.com>
2026-09-11 23:44:46 +0530

    Configure frontend API for deployment

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,22 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "/api"
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-);
 
-export default api;
+    return config;
+});
+
+export default API;
