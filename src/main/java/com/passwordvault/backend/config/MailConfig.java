f28b7fd249f7e405226fc7ae065072a58d4ab870
Ramya Sri <rramyasrikadimi@gmail.com>
2026-09-11 23:33:16 +0530

    Fix mail sender configuration

---
+++ src/main/java/com/passwordvault/backend/config/MailConfig.java
@@ -0,0 +1,34 @@
+package com.passwordvault.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(System.getenv("MAIL_USERNAME"));
+        mailSender.setPassword(System.getenv("MAIL_PASSWORD"));
+
+        mailSender.getJavaMailProperties().put(
+                "mail.smtp.auth",
+                "true"
+        );
+
+        mailSender.getJavaMailProperties().put(
+                "mail.smtp.starttls.enable",
+                "true"
+        );
+
+        return mailSender;
+    }
+}
